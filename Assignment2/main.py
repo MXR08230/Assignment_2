dce3a9f264a1e2dfb0e000cbf124bb09254a8b54
Mounika <MXR08230@ucmo.edu>
2022-10-20 20:59:25 -0500

    updated

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -1,14 +1,1 @@
-import numpy as np
-a=np.random.randint(1,20,15)
-print("array")
-print(a)
-a=a.reshape(3,5)
-print("reshapped array")
-print(a)
-print("array shape")
-print(a.shape)
-print("max of array")
-print(a.max())
-print("array with replaced max value")
-a[a==a.max()]=0
-print(a)
+hi
